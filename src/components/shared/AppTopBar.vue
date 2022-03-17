699c78cc31e2d5a2901ca7f0cadb35dfc2ff5dae
Ricardo <ricardomr8@hotmail.com>
2022-03-17 20:00:54 -0300

    primeiro projeto vue

---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="app-top-bar-events">
+    <v-app id="inspire">
+      <v-app-bar app absolute color="#43a047" dark prominent src="soccer.jpg">
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+          ></v-img>
+        </template>
+
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-app-bar-title>Primeiro-projeto</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-col cols="auto" sm="6" md="3">
+          <v-text-field
+            placeholder="Search"
+            prepend-inner-icon="mdi-magnify"
+            class="expanding-search md-1 closed"
+            filled
+            dense
+          ></v-text-field>
+        </v-col>
+      </v-app-bar>
+      <v-navigation-drawer v-model="drawer" app>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6"> Vuetify </v-list-item-title>
+            <v-list-item-subtitle> for Iteris </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            :to="item.to"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "AppTopBar",
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "That's it", icon: "mdi-format-list-checks", to: "/" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
+    ],
+    right: null,
+  }),
+};
+</script>
+    
+    <style scoped>
+.app-top-bar a {
+  text-decoration: none;
+}
+
+</style>
---
+++ src/views/AboutUs.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home pa-6">
+    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, eligendi! Officia voluptate quibusdam qui ipsa deserunt doloribus dolor! Nisi alias itaque distinctio? 
+        Tempora ullam facilis, labore nam odio rem esse. Numquam obcaecati qui, veniam rerum culpa aut vero explicabo vitae tenetur excepturi eius aperiam ipsam reprehenderit 
+        ut nisi autem cum alias nostrum nam amet aliquam. Exercitationem incidunt rerum illo distinctio, necessitatibus laudantium ipsum quibusdam non assumenda, saepe facilis 
+        sequi accusantium eaque quae. Expedita commodi beatae eos ullam quasi suscipit cumque fugit, praesentium recusandae alias doloribus possimus non quod 
+        asperiores ad dolores corrupti magnam cum ex architecto culpa obcaecati esse magni.</h1>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
